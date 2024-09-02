54a6844e9e650665aac247b6255826c0d84b92dd
ejd79 <erickjd79@gmail.com>
2024-09-02 12:19:57 +0200

    refactor code to externalize the topic name removing hardcoded value

--- src/main/java/net/javaguides/springboot/kafka/KafkaProducer.java
+++ src/main/java/net/javaguides/springboot/kafka/KafkaProducer.java
@@ -2,6 +2,7 @@ package net.javaguides.springboot.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 
@@ -20,7 +24,7 @@ public class KafkaProducer {
 
     public void sendMessage(String message) {
         LOGGER.info(String.format("Message sent %s", message));
-        kafkaTemplate.send("javaguides", message);
+        kafkaTemplate.send(topicName, message);
     }
 
 }
